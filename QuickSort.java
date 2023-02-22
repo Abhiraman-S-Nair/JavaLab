a6909e8c5464e079815fd9df63b78f424b32b030
Abhiraman-S-Nair <84242314+Abhiraman-S-Nair@users.noreply.github.com>
2023-02-22 07:56:37 +0530

    Update QuickSort.java

--- QuickSort.java
+++ QuickSort.java
@@ -12,7 +12,7 @@ class Quicksort
 	
 	static int partition(int[] arr, int low, int high)
 	{
-		int pivot = arr[high];
+		int pivot = arr[low];
 		/*
 		 * int i = (low - 1); for (int j = low; j <= high - 1; j++) { if (arr[j] <
 		 * pivot) { i++; swap(arr, i, j); } }
